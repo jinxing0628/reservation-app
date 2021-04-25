7806ff61fac03c8798519ed83170c47a986f50ac
jinxing <kimsung0628@gmail.com>
2021-04-26 00:13:41 +0900

    hirokuセットアップ

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config/index')
 const FakeDb = require('./fake-db')
 
 const productRouters = require('./routes/products')
+const path = require('path')
 
  mongoose.connect(config.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(
       () => {
+        if(process.env.NODE_ENV !== 'production'){
           const fakeDb = new FakeDb()
-          fakeDb.initDb()
+          // fakeDb.initDb()
         //   fakeDb.seeDb()
+        }
       }
   )
 // await mongoose.connect(config.DB_URI, {
@@ -25,6 +28,15 @@ const productRouters = require('./routes/products')
 const app = express()
 
 app.use('/api/v1/products',productRouters)
+
+if(process.env.NODE_ENV === 'production'){
+  const appPath = path.join(__dirname, '..','dist','reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function(req,res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+
+}
 
 // app.get('/products',function(req,res){
 //     res.json({'sucess':true})
